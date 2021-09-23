410d8b8ad0abd2ff57f937f14db178250a8ef02f
jinxluck <54708834+jinxluck@users.noreply.github.com>
2021-09-23 09:38:18 +0200

    login required decorated made and tested

--- django-website/website/field_user/views.py
+++ django-website/website/field_user/views.py
@@ -1,15 +1,19 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
+@login_required
 def field_user_home(request):
 
     return render(request, "field_user_home.html")
 
+@login_required
 def field_user_info(request):
 
     return render(request, "field_user_info.html")
 
+@login_required
 def field_user_access(request):
 
     return render(request, "field_user_access.html")
