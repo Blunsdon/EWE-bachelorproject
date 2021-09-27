cdd205f1a794e5d821a298cf306fbd7940a4fac3
Blunsdon <blunsdon@hotmail.dk>
2021-09-27 11:03:44 +0200

    Field user info page updated
    - CSS added
    - tables added
    - placeholder added

--- django-website/website/field_user/views.py
+++ django-website/website/field_user/views.py
@@ -1,19 +1,29 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from website.forms import *
 
 # Create your views here.
+
+
 
 @login_required
 def field_user_home(request):
 
     return render(request, "field_user_home.html")
 
+# get user info
 @login_required
 def field_user_info(request):
-
-    return render(request, "field_user_info.html")
+    name = request.user.name
+    phone = request.user.phoneNumber
+    email = request.user.email
+    company = request.user.company
+    password = request.user.password
+    dict = {'name': name, 'email': email, 'phone': phone, 'company': company, 'password': password}
+    return render(request, "field_user_info.html", dict)
 
 @login_required
 def field_user_access(request):
 
     return render(request, "field_user_access.html")
+
