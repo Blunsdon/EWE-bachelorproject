ea0361318694554f9134631b013f313d17f99c04
Blunsdon <blunsdon@hotmail.dk>
2021-09-30 15:15:07 +0200

    Create user
    - Create user implemented
    - Create user code generator done
    - Office user can now see create user code

--- django-website/website/create_user/views.py
+++ django-website/website/create_user/views.py
@@ -1,11 +1,27 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from website.models import Users, CreateUserCode
+from website.forms import CreateNewUserForm
 
-# Home for create user
+
+
 def create_user(request):
+    """
+    Home for create user
+    :param request:
+    :return:
+    """
+    if request.method == 'POST':
+        form = CreateNewUserForm(request.POST)
+        if form.is_valid():
+            cu = request.POST.get('CreateUserCode')
+            cuc = CreateUserCode.objects.get(id=1)
+            if cu == cuc.code:
+                form.save()
+                return redirect('/accounts/login/')
+    else:
+        form = CreateNewUserForm()
+    return render(request, "create_user.html", {'form': form})
 
-    return render(request, "create_user.html")
-
-# Create your views here.
-# TODO: review
 
