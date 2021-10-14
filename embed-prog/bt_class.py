413f41457f7d609090844e72451c4f8cfa200220
jinxluck <54708834+jinxluck@users.noreply.github.com>
2021-10-14 14:23:20 +0200

    embed bluetooth and gpio controlling program "done"

---
+++ embed-prog/bt_class.py
@@ -0,0 +1,62 @@
+import bluetooth
+import time
+from lock_control import lock_control
+
+lock_control = lock_control()
+msg = 42
+time.sleep(3)
+
+server_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+server_sock.bind(("", bluetooth.PORT_ANY))
+server_sock.listen(1)
+
+port = server_sock.getsockname()[1]
+
+uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
+
+bluetooth.advertise_service(server_sock, "SampleServer", service_id=uuid,
+                            service_classes=[uuid, bluetooth.SERIAL_PORT_CLASS],
+                            profiles=[bluetooth.SERIAL_PORT_PROFILE],
+                            # protocols=[bluetooth.OBEX_UUID]
+                            )
+
+print("Waiting for connection on RFCOMM channel", port)
+
+try:
+    while True:
+        client_sock, client_info = server_sock.accept()
+        print("Accepted connection from", client_info)
+        try:
+            while True:
+                msg = client_sock.recv(1024)
+                msg = str(msg)
+                msg = msg.replace("'", "")
+                msg = msg[1:]
+                print(msg)
+                print(isinstance(msg, str))
+                if lock_control.validate_key(msg):
+                    "Great open lock"
+                    print("Key is valid lock is now unlocked")
+                    if lock_control.unlock_lock():
+                        "The lock is now unlocked for 30 sec"
+                        time.sleep(5)
+                        if lock_control.lock_lock():
+                            print("The lock is now locked")
+                            "Great lock is locked"
+                        else:
+                            "lock failed"
+                    else:
+                        "lock failed to unlock"
+                else:
+                    client_sock.close()
+                    print("Key is no good")
+        except OSError:
+            pass
+except OSError:
+    pass
+
+print("Disconnected.")
+
+client_sock.close()
+server_sock.close()
+print("All done.")
--- embed-prog/lock_control.py
+++ embed-prog/lock_control.py
@@ -1,34 +1,39 @@
 import RPi.GPIO as GPIO
 import time
 
-signal = 23
-key_code = 42
+class lock_control(object):
+    signal = 23
+    key_code = str(42)
+
+    def __init__(self):
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.signal, GPIO.OUT)
+        GPIO.output(self.signal, GPIO.HIGH)
+
+    def validate_key(self, msg):
+        if msg == self.key_code:
+            bol = True
+        else:
+            bol = False
+        return bol
 
 
-def validate_key(msg):
-    if msg == key_code:
-        bol = True
-    else:
-        bol = False
-    return bol
+    def unlock_lock(self):
+        GPIO.output(self.signal, GPIO.LOW)
+        if GPIO.input(self.signal) == 0:
+            bol = True
+        else:
+            bol = False
+        return bol
 
 
-def unlock_lock():
-    GPIO.output(signal, GPIO.LOW)
-    if GPIO.input(signal) == 0:
-        bol = True
-    else:
-        bol = False
-    return bol
-
-
-def lock_lock():
-    GPIO.output(signal, GPIO.HIGH)
-    if GPIO.input(signal) == 1:
-        bol = True
-    else:
-        bol = False
-    return bol
+    def lock_lock(self):
+        GPIO.output(self.signal, GPIO.HIGH)
+        if GPIO.input(self.signal) == 1:
+            bol = True
+        else:
+            bol = False
+        return bol
 
 
 
