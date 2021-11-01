6735cf35e2d9f2099eea198ab9484452e3546cbc
Blunsdon <blunsdon@hotmail.dk>
2021-11-01 21:51:01 +0100

    Login fragment
    The App can now:
    - request login
    - check login
    - send login info to next fragment
    - handle failed login

--- main/java/com/example/controllockbt/ScanFrag.kt
+++ main/java/com/example/controllockbt/ScanFrag.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 
 
 class ScanFrag : Fragment() {
@@ -15,7 +16,32 @@ class ScanFrag : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_scan, container, false)
+        val view = inflater.inflate(R.layout.fragment_scan, container, false)
+
+
+        //Get arguments
+        val args = this.arguments
+        //get a specific data entry in the bundle
+        val inputData = args?.get("useremail")
+        val inputData2 = args?.get("password")
+        val token = args?.get("token")
+
+        //display bundle data
+        val tokenString = inputData.toString()
+        val tokenString2 = inputData2.toString()
+        val tokenString3 = token.toString()
+
+        val viewtest = view.findViewById<TextView>(R.id.textView)
+        val viewtest2 = view.findViewById<TextView>(R.id.textView2)
+        val textToken = view.findViewById<TextView>(R.id.textToken)
+
+        viewtest.text = tokenString
+        viewtest2.text = tokenString2
+        textToken.text = tokenString3
+
+        return view
     }
+
+
 
 }
