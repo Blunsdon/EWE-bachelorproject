94dc7ee35ef97c94ae823cc1438f1f84cc7baaf4
jinxluck <54708834+jinxluck@users.noreply.github.com>
2021-12-08 13:22:28 +0100

    Update UnlockFrag.kt
    removed warnings

--- main/java/com/example/controllockbt/UnlockFrag.kt
+++ main/java/com/example/controllockbt/UnlockFrag.kt
@@ -1,5 +1,6 @@
 package com.example.controllockbt
 
+import android.annotation.SuppressLint
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothManager
@@ -17,7 +18,6 @@ import android.widget.Toast
 import androidx.activity.addCallback
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.Navigation
-import androidx.navigation.fragment.findNavController
 import com.example.controllockbt.activities.SendLog.SendLogModelFactory
 import com.example.controllockbt.activities.SendLog.SendLogViewModel
 import com.example.controllockbt.model.Facility
@@ -27,21 +27,22 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-import okio.ByteString.Companion.readByteString
-import java.io.BufferedReader
 import java.io.IOException
 import java.time.Instant
 import java.time.format.DateTimeFormatter
 import java.util.*
-import java.util.logging.Logger.global
+
+/*
+This class might need a thorough cleanup and refactoring.
+ */
 
 class UnlockFrag : Fragment() {
 
     //Bundle var's
-    private lateinit var Token: String
-    private lateinit var UserEmail: String
-    private lateinit var FacName: String
-    private lateinit var MacAdress: String
+    private lateinit var token: String
+    private lateinit var userEmail: String
+    private lateinit var facName: String
+    private lateinit var macAdress: String
 
     //Bluetooth var's
     private var mMyUUID: UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB")
@@ -72,17 +73,15 @@ class UnlockFrag : Fragment() {
         //Get arguments
         val args = this.arguments
         //get a specific data entry in the bundle
-        Token = args?.get("Token").toString()
-        UserEmail = args?.get("UserEmail").toString()
-        FacName = args?.get("FacName").toString()
-        MacAdress = args?.get("MacAdress").toString()
+        token = args?.get("Token").toString()
+        userEmail = args?.get("UserEmail").toString()
+        facName = args?.get("FacName").toString()
+        macAdress = args?.get("MacAdress").toString()
 
         sendLog()
 
         //response okay -> launch coroutines
-        var job = CoroutineScope(Dispatchers.IO).launch {
-            connectBt()
-        }
+        CoroutineScope(Dispatchers.IO).launch { connectBt() }
 
         // This callback will only be called when MyFragment is at least Started.
         requireActivity().onBackPressedDispatcher.addCallback(this) {
@@ -94,7 +93,6 @@ class UnlockFrag : Fragment() {
     }
 
     private fun sendLog(){
-        var logReturn: Boolean
         val repository = Repository()
         //retrofit modelFac
         val viewModelFactory = SendLogModelFactory(repository)
@@ -102,11 +100,11 @@ class UnlockFrag : Fragment() {
         viewModel = ViewModelProvider(this, viewModelFactory).get(SendLogViewModel::class.java)
         //push POST to restAPI
         val tokenPlace = "Token "
-        val tokenString = tokenPlace.plus(Token)
+        val tokenString = tokenPlace.plus(token)
 
-        var fac: List<Facility> = listOf(Facility(FacName))
-        var log: List<com.example.controllockbt.model.Log> = listOf(com.example.controllockbt.model.Log(DateTimeFormatter.ISO_INSTANT.format(
-            Instant.now()), UserEmail, FacName))
+        val fac: List<Facility> = listOf(Facility(facName))
+        val log: List<com.example.controllockbt.model.Log> = listOf(com.example.controllockbt.model.Log(DateTimeFormatter.ISO_INSTANT.format(
+            Instant.now()), userEmail, facName))
         val myPost = PostSendLog(fac, log,"")
 
         viewModel.pushPost(tokenString, myPost)
@@ -117,8 +115,6 @@ class UnlockFrag : Fragment() {
                 keyString = response.body()?.key.toString()
 
                 logSucces = true
-            } else {
-                //no response
             }
         })
     }
@@ -129,14 +125,15 @@ class UnlockFrag : Fragment() {
         }
     }
 
+    @SuppressLint("SetTextI18n")
     private fun setNewStatus(status: String, code: String){
         val progressBar: ProgressBar? = view?.findViewById(R.id.progressBarUnlock)
         val textCode: TextView? = view?.findViewById(R.id.textViewUnlock)
         if(status == "done"){
-            var bundle = Bundle()
-            bundle.putString("Token", Token)
-            bundle.putString("UserEmail", UserEmail)
-            bundle.putString("FacName", FacName)
+            val bundle = Bundle()
+            bundle.putString("Token", token)
+            bundle.putString("UserEmail", userEmail)
+            bundle.putString("FacName", facName)
             bundle.putString("StatusCode", code)
             view?.let { Navigation.findNavController(it).navigate(R.id.action_unlockFrag_to_successFrag, bundle)}
         } else {
@@ -145,18 +142,18 @@ class UnlockFrag : Fragment() {
         }
     }
 
-    private fun sendCommand(input: String, view: View): String {
+    private fun sendCommand(): String {
         Log.d("SCCode", keyString)
         if(mBluetoothSocket != null){
             try {
-                if(mBluetoothSocket!!.isConnected == true) {
-                    var loopControl: Boolean = false
-                    var msgControl: Boolean = false
-                    var loopCount: String = "0"
+                if(mBluetoothSocket!!.isConnected) {
+                    val loopControl = false
+                    var msgControl = false
+                    var loopCount = "0"
                     while(!loopControl){
                         if(logSucces) {
                             if(!msgControl) {
-                                Log.d("sendCommand", "Sending msg: " + keyString)
+                                Log.d("sendCommand", "Sending msg: $keyString")
                                 mBluetoothSocket!!.outputStream.write(keyString.toByteArray())
                                 msgControl = true
                             }
@@ -164,7 +161,7 @@ class UnlockFrag : Fragment() {
                             if(mBluetoothSocket!!.inputStream.available().toString() != "0") {
                                 Log.d("input_stream_after", mBluetoothSocket!!.inputStream.available().toString())
                                 mBluetoothSocket!!.inputStream.read(mmBuffer)
-                                var readMsg = String(mmBuffer)
+                                val readMsg = String(mmBuffer)
                                 Log.d("input_stream", readMsg)
                                 if (readMsg.contains("200")) {
                                     cleanSocket()
@@ -198,7 +195,7 @@ class UnlockFrag : Fragment() {
 
     private fun cleanSocket(){
         Log.d("CloseSocket_sc", "closing socket connections")
-        if(mBluetoothSocket!!.isConnected == true) {
+        if(mBluetoothSocket!!.isConnected) {
             try {
                 Log.d("CloseSocket_sc", "closing s.input: " + mBluetoothSocket?.inputStream?.available())
                 mBluetoothSocket?.inputStream?.close()
@@ -232,7 +229,7 @@ class UnlockFrag : Fragment() {
                     context?.getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
                 bluetoothManager.adapter.cancelDiscovery()
                 mBluetoothAdapter = bluetoothManager.adapter
-                val device: BluetoothDevice = mBluetoothAdapter.getRemoteDevice(MacAdress)
+                val device: BluetoothDevice = mBluetoothAdapter.getRemoteDevice(macAdress)
                 kotlin.runCatching {
                     mBluetoothSocket = device.createInsecureRfcommSocketToServiceRecord(mMyUUID)
                 }
@@ -243,9 +240,9 @@ class UnlockFrag : Fragment() {
                     navigate("500")
                 }
                 mIsConnected = true
-                var SCstring: String? = view?.let { sendCommand("42", it) }
-                if (SCstring != null) {
-                    editMainThread("done", SCstring)
+                val scString: String? = view?.let { sendCommand() }
+                if (scString != null) {
+                    editMainThread("done", scString)
                 } else {
                     editMainThread("done", "500")
                 }
@@ -259,11 +256,12 @@ class UnlockFrag : Fragment() {
         }
     }
 
+    //use this instead of fin setNewStatus?
     private fun navigate(status: String){
-        var bundle = Bundle()
-        bundle.putString("Token", Token)
-        bundle.putString("UserEmail", UserEmail)
-        bundle.putString("FacName", FacName)
+        val bundle = Bundle()
+        bundle.putString("Token", token)
+        bundle.putString("UserEmail", userEmail)
+        bundle.putString("FacName", facName)
         bundle.putString("StatusCode", status)
         view?.let { Navigation.findNavController(it).navigate(R.id.action_unlockFrag_to_successFrag, bundle) }
     }
